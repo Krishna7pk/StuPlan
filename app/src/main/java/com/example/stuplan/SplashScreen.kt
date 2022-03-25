2a6b4c635aca1377fe3a81b598898d5d34e17f04
Sajan Tamang <sajantmg2@gmail.com>
2022-03-25 11:11:55 -0400

    commit from project

---
+++ app/src/main/java/com/example/stuplan/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.stuplan
+
+import android.content.Intent
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.stuplan.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }, 3000)
+
+    }
+}
