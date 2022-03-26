e17bcef0486f5d01de60628a39b798eabb17b1a1
Sajan Tamang <sajantmg2@gmail.com>
2022-03-26 13:24:03 -0400

    added more codes

--- app/src/main/java/com/example/stuplan/MainActivity.kt
+++ app/src/main/java/com/example/stuplan/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.stuplan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.stuplan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val taskFragment = TasksFragment()
+        val completedFragment = CompletedFragment()
+        val coursesFragment = CoursesFragment()
+        val settingsFragment = SettingsFragment()
+
+        changeFragment(taskFragment)
+
+        binding.bottomNav.setOnItemSelectedListener {menuitem->
+            when(menuitem.itemId){
+                R.id.tasks -> changeFragment(taskFragment)
+                R.id.completed -> changeFragment(completedFragment)
+                R.id.courses -> changeFragment(coursesFragment)
+                R.id.settings -> changeFragment(settingsFragment)
+            }
+            true
+        }
+
+
+
+
+    }
+
+    fun changeFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.host_fragment, fragment)
+            commit()
+        }
     }
 }
